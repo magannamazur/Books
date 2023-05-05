6b2563869ae012f90c25b382fd527e15f6741c0e
Magdalena Mazur <mag.anna.mazur@gmail.com>
2023-05-05 23:41:27 +0200

    basics

--- Basics/input.py
+++ Basics/input.py
@@ -1,3 +1,4 @@
+# funkcja input zwraca stringa
 input("\n\nAby ropocząć program, naciśnij klawisz Enter.")
 
 # \n - znak przejscia do nowej lini
@@ -6,3 +7,6 @@ name = input("Cześć. Jak masz na imię? ")
 print(name)
 print("Cześć,", name)
 input("\n\nAby zakończyć program, naciśnij klawisz Enter.")
+
+# zmiana typu wprowadzonych danych
+jet = int(input("Podaj cene wynajmu prywatnego samolotu: "))
