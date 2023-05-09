828460711e40c835d4a488865314de8d3dca7128
Magdalena Mazur <mag.anna.mazur@gmail.com>
2023-05-09 22:48:02 +0200

    random

--- modules/randomm.py
+++ modules/randomm.py
@@ -3,5 +3,6 @@ import random
 # generuj liczby losowe z zakresu 1 - 6
 die1 = random.randint(1, 6)
 die2 = random.randrange(6) + 1 # liczbę całkowitą z przedziału od 0 do górnej granicy z jej wyłączeniem
+die3 = random.randrange(5,7) # dostepne 5,6
 
-print(die1, die2)
+print(die1, die2, die3)
