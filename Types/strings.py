b9e9dc709180e01054eb123a7a45ae1ae40e8803
Magdalena Mazur <mag.anna.mazur@gmail.com>
2023-05-09 23:14:18 +0200

    type

--- Types/strings.py
+++ Types/strings.py
@@ -1,4 +1,5 @@
 # metody łańcuchów
+# niemutowalne
 # Cytat z wypowiedzi prezesa IBM, Thomasa Watsona, z 1943 r.
 quote = "Myślę, że istnieje światowy rynek dla może pięciu komputerów."
 print("Oryginalny cytat w tłumaczeniu na język polski:")
@@ -30,3 +31,11 @@ print(len("ds"))
 
 # indeksowanie od 0
 print("word[", 3, "]\t", word[3])
+print("word[ 2:4 ]\t", word[2:4])  # 2=m, 3=e, 4 jest wylaczone
+print("word[ 0:4 ]\t", word[:4])  # 0=S, 1=o, 2=m, 3=e, 4 jest wylaczone
+print("word[ : ]\t", word[:])  # all
+
+
+# None daje wartość False, ma type NoneType
+sth = None
+print(type(sth))
