bc32a1d1d237712ca0299483b994f4e3f6d8a853
Magdalena Mazur <mag.anna.mazur@gmail.com>
2023-05-17 15:05:16 +0200

    game

--- Games/najlepsze_wyniki.py
+++ Games/najlepsze_wyniki.py
@@ -1,7 +1,7 @@
 scores = []
 choice = None
 while choice != "0":
-print(
+    print(
 """
 Najlepsze wyniki
 0 - zakończ
@@ -9,8 +9,25 @@ Najlepsze wyniki
 2 - dodaj wynik
 """
 )
-choice = input("Wybierasz: ")
-print()
+    choice = input("Wybierasz: ")
+    print()
 # zakończ
-if choice == "0":
-print("Do widzenia.")
+    if choice == "0":
+        print("Do widzenia.")
+
+# wyświetl tabelę najlepszych wyników
+    elif choice == "1":
+        print("Najlepsze wyniki\n")
+        print("GRACZ\tWYNIK")
+        for entry in scores:
+            score, name = entry
+            print(name, "\t", score)
+
+# add a score
+    elif choice == "2":
+        name = input("Podaj nazwę gracza: ")
+        score = int(input("Jaki wynik gracz uzyskał?: "))
+        entry = (score, name)
+        scores.append(entry)
+        scores.sort(reverse=True)
+        scores = scores[:5]  # zachowaj tylko 5 najlepszych wyników
