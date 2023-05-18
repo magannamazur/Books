6a50b1d5b7dc4712cdeb13f59d4f52e74d9380d2
Magdalena Mazur <mag.anna.mazur@gmail.com>
2023-05-18 23:43:54 +0200

    function

---
+++ Others/functions.py
@@ -0,0 +1,7 @@
+# łańcuch dokomentujący, docstring
+def instructions():
+    """Wyświetl instrukcję gry."""
+    print("Witaj w największym intelektualnym wyzwaniu wszech czasów")
+
+# wywołanie funkcji
+instructions()
