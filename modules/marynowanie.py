5d01d1973298d66bf67a5be91992137d0dddedcd
Magdalena Mazur <mag.anna.mazur@gmail.com>
2023-05-21 22:49:18 +0200

    modules

---
+++ modules/marynowanie.py
@@ -0,0 +1,6 @@
+import pickle, shelve
+
+# Moduł pickle umożliwia marynowanie i przechowywanie w pliku bardziej złożonych
+# danych. Moduł shelve umożliwia magazynowanie zamarynowanych obiektów w pliku
+# i zapewnia do nich dostęp swobodny.
+# Marynowanie jest proste. Zamiast
