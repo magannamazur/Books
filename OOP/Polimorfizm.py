866d81f021552be9f70e0f8a47391e55ff34a218
Magdalena Mazur <mag.anna.mazur@gmail.com>
2023-06-11 21:07:04 +0200

    OOP

---
+++ OOP/Polimorfizm.py
@@ -0,0 +1,7 @@
+# Polimorfizm to zdolność do traktowania różnego typu rzeczy tak samo i sprawienia,
+# aby każda z nich reagowała na swój własny sposób. W kontekście programowania
+# obiektowego polimorfizm oznacza, że możesz wysłać ten sam komunikat do obiektów
+# różnych klas powiązanych poprzez dziedziczenie oraz osiągnąć różne, odpowiednie dla
+# konkretnego obiektu wyniki. Kiedy na przykład wywołasz metodę __str__() obiektu
+# klasy Unprintable_Card, która jest klasą pochodną klasy Card, otrzymasz inny wynik niż
+# wtedy, kiedy wywołasz metodę __str__() obiektu klasy Card.
